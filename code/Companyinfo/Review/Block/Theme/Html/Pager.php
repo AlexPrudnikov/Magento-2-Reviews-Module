3fbf54d1d06dcdd75036c1326b273614cee670fe
Alexsander <RA.Residentadvisor@gmail.com>
2020-12-20 19:20:36 +0200

    Delete Pager.php

--- code/Companyinfo/Review/Block/Theme/Html/Pager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Companyinfo\Review\Block\Theme\Html;
-
-
-class Pager extends \Magento\Theme\Block\Html\Pager
-{
-    protected $_template = 'Companyinfo_Review::html/pager.phtml';
-}
